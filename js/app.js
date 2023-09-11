e589cb3c534a112efcd24ad28adf1f9a47e6a759
Telianakai <123404470+Telianakai@users.noreply.github.com>
2023-09-11 13:03:22 -0400

    Update app.js (Added nav highlight on click and scroll functionality)

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
-
-//Global Variables_____________________________________________________________________________
+// Global Variables_____________________________________________________________________________
 
 /* global document */
 /* global window */
 /* global clearTimeout */
 /* global setTimeout */
 
-//Section Data Structure_______________________________________________________________________
+// Section Data Structure_______________________________________________________________________
 
 // Get all the sections using querySelectorAll
 const sections = document.querySelectorAll("section");
@@ -25,9 +24,9 @@ sections.forEach((section) => {
   });
 });
 
-//Build the Navigation_________________________________________________________________________
+// Build the Navigation_________________________________________________________________________
 
-const navbarList = document.getElementById("navbar__list");
+const navbarList = document.getElementById('navbar__list');
 // Function to generate navigation links dynamically based on the sections
 
 function createNavigationLinks() {
@@ -48,7 +47,7 @@ function createNavigationLinks() {
 // Call the Navigation list function when the page loads
 createNavigationLinks();
 
-//Scroll to Section Functionality______________________________________________________________
+// Scroll to Section Functionality______________________________________________________________
 
 // Function to scroll to the target section when a navigation link is clicked
 function scrollToSection(event) {
@@ -59,16 +58,21 @@ function scrollToSection(event) {
 
     // Scroll to the target section smoothly
     targetSection.scrollIntoView({ behavior: "smooth" });
+
+    // Remove the "active" class from all sections
+    sections.forEach((section) => {
+      section.classList.remove("active-section");
+    });
+
+    // Add the "active" class to the target section
+    targetSection.classList.add("active-section");
   }
 }
 
 // Event listener for the navbarList to handle navigation clicks
 navbarList.addEventListener("click", scrollToSection);
 
-//Active section functionality_________________________________________________________________
-
-// CSS class to style the active section
-const activeClass = "active-section";
+// Active section functionality_________________________________________________________________
 
 // Function to check if a section is in the viewport
 function isSecInVP(section) {
@@ -85,12 +89,26 @@ function isSecInVP(section) {
 function updateActive() {
   sections.forEach((section) => {
     if (isSecInVP(section)) {
-      section.classList.add(activeClass);
-      scrollToSection(section);
+      section.classList.add("active-section");
     } else {
-      section.classList.remove(activeClass);
+      section.classList.remove("active-section");
     }
   });
+
+  // Update the active menu link based on the active section
+  const activeSection = document.querySelector(".active-section");
+  if (activeSection) {
+    const activeLinkId = activeSection.getAttribute("id");
+    const activeLink = navbarList.querySelector(`a[href="#${activeLinkId}"]`);
+
+    // Remove the "active" class from all menu links
+    navbarList.querySelectorAll("a").forEach((link) => {
+      link.classList.remove("active");
+    });
+
+    // Add the "active" class to the active menu link
+    activeLink.classList.add("active");
+  }
 }
 
 // Initial call to set the active section on page load
@@ -99,7 +117,7 @@ updateActive();
 // Scroll event listener to updating active section
 window.addEventListener("scroll", updateActive);
 
-//Hide & Reveal Navbar_________________________________________________________________________
+// Hide & Reveal Navbar_________________________________________________________________________
 
 let timeoutId; // Variable to store the timeout ID
 
@@ -110,17 +128,16 @@ function showNavbar() {
   // Clear the previous timeout (if any) and set a new timeout
   clearTimeout(timeoutId);
 
-  //Hide the navbar again after 3 seconds of inactivity
+  // Hide the navbar again after 3 seconds of inactivity
   timeoutId = setTimeout(function () {
     navbarList.style.display = "none";
   }, 3000);
 }
 
-// Event listener to show navbar when user scrolls
+// Event listener to show navbar when the user scrolls
 window.addEventListener("scroll", function () {
   showNavbar();
 });
 
 // Call showNavbar to initially display the navbar
 showNavbar();
-
